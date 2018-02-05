c5621f87bb2870aa85a7ed0a3c7427c781ae3c4d
tafitzs <tafitzs@hawaii.edu>
2018-02-05 13:49:51 -1000

    Completed variable coding challenge #1

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -5,5 +5,23 @@ import UIKit
 var str = "Hello, playground"
 
 //Variable Coding Challenge #1
-var firstString = "Happy"
+
+var firstString = "I am happy"
+
+let secondString = " Because I am Having a Good Day."
+
+var thirdString = firstString + secondString
+
+print(thirdString)
+
+//Variable Coding Challenge #2
+let C = "celsius"
+let (fahrenheit - 32)/1.8
+let fahrenheit = 50
+print("Today is celsisus )
+
+
+
+
+
 
