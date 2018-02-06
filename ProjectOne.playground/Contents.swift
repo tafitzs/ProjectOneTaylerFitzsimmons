08df2d6794808b5318d69a44cf32bf20ba3bc0c3
tafitzs <tafitzs@hawaii.edu>
2018-02-06 12:33:04 -1000

    Completed If Statement Coding Challenge #1

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -4,8 +4,8 @@ import UIKit
 
 var str = "Hello, playground"
 
-//Variable Coding Challenge #1
 
+//Variable Coding Challenge #1
 var firstString = "I am happy"
 
 let secondString = " Because I am Having a Good Day."
@@ -14,20 +14,39 @@ var thirdString = firstString + secondString
 
 print(thirdString)
 
-//Variable Coding Challenge #2
 
+//Variable Coding Challenge #2
 let fahrenheit: Float = 50
 
 let celsius = (fahrenheit-32)*5/9
 
 print("Today is \(celsius) degrees celsius.")
 
+
 //Boolean Variables Coding Challenge #1
 var numberOne = 20
 var numberTwo = 10
 var numberThree = numberOne == numberTwo
-print(numberThree)
 
+
+// If Statement coding Challenge #1
+var boxOne = "Miami"
+var boxTwo = "Malibu"
+
+let lengthBoxOne = boxOne.characters.count
+let lengthBoxTwo = boxTwo.characters.count
+
+if boxOne.characters.count > boxTwo.characters.count {
+    print("Yes")
+    print("\(lengthBoxOne) > \(lengthBoxTwo)")
+    print("Miami has more characters")
+    
+}
+else {
+    print("No")
+    print("\(lengthBoxOne) < \(lengthBoxTwo)")
+    print("Miami does not have more characters")
+}
 
 
 
